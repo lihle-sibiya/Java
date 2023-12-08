ec060dcaa1d52ece5dec99b1745e4b4ac8a112d7
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-12-08 14:00:54 +0200

    11th commit

---
+++ Les3/src/lesson9/Item.java
@@ -0,0 +1,19 @@
+package lesson8;
+
+import ex06_2_exercise.*;
+
+public class Item {
+        public int itemID;
+        public String desc;
+        public double price;
+        public int quantity;
+        public String size;
+  
+        
+        public void setSize(String sizeArg){
+        this.size = sizeArg;
+        }
+           
+        
+}
+
