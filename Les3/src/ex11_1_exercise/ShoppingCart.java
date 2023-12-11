90aea795bb1de1faef164583945a97d03d150703
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-12-11 23:26:05 +0200

    18th commit

--- Les3/src/ex11_1_exercise/ShoppingCart.java
+++ Les3/src/ex11_1_exercise/ShoppingCart.java
@@ -1,12 +1,3 @@
-package ex11_1_exercise;
-
-//Exercise 11-1
-
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-
-
-//
 //1. Declare a LocalDateTime object to hold the order date.
 //2. Initialze the object to the current date and time using the 
 //     now static method of the class.
@@ -16,41 +7,31 @@ import java.time.format.DateTimeFormatter;
 //5. Add the necessary package imports (hint: java.time.<classname>)
 //6. Print the formatted orderDate with a suitable label 
 //7. Run your code.
+package ex11_1_exercise;
 
 // import statements here:
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 
 public class ShoppingCart {
     public static void main(String[] args){
 	// Declare a LocalDateTime object, orderDate
-LocalDateTime orderDate;
+
+        LocalDateTime orderDate = LocalDateTime.now();
+        String formatOrderDate = orderDate.format(DateTimeFormatter.ISO_DATE);
+        System.out.println("Order date fomatted is "+ formatOrderDate);
+        
+        String sDate = orderDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println("Iso local date "+sDate);
         
 	// Initialize the orderDate to the current date and time. Print it.
-orderDate = LocaDateTime.now();
-        System.out.println("Order date is: " + orderDate);
+
 
 	// Format orderDate using ISO_LOCAL_DATE; Print it.
-String oDate = orderDate.format(DateTimeFormater.ISO__LOCAL_DATE);
-        System.out.println("ISO DATE is: " + odate);
 
-    }
 
-    private static class LocaDateTime {
-
-        private static LocalDateTime now() {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
-
-        public LocaDateTime() {
-        }
-    }
-
-    private static class DateTimeFormater {
-
-        private static DateTimeFormatter ISO__LOCAL_DATE;
-
-        public DateTimeFormater() {
-        }
     }
 }
